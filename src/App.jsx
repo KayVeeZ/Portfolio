83051326387b9104d624af8d0e39a561f5948884
KayVeeZ <kshitijvashisth@gmail.com>
2025-03-29 15:17:35 +0000

    added navbar and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import './App.css';
 import './Cursors.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-    <button className="z-1 custom-green text-white font-bold py-2 px-4 rounded">Hello</button>
+      <div className=" flex text-white flex-col min-h-screen justify-between">
+        <Navbar />
+        <div className='flex justify-center items-center'>
+        <button className="flex z-1 custom-green text-white font-bold py-2 px-4 rounded">Hello</button>
+        </div>
+        <Footer />
+      </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import MatrixRainingCode from './components/MatrixRainingCode.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div style={{ position: 'absolute', width: '100vw', height: '100vh', zIndex: '-1'}}>
