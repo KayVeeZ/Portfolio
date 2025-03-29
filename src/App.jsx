576376a3520af12787588b8c1d262382427f8c1d
KayVeeZ <kshitijvashisth@gmail.com>
2025-03-29 15:05:12 +0000

    moved matrix rain effect to main.jsx for better code writing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react';
-import MatrixRainingCode from './components/MatrixRainingCode';
 import './App.css';
 import './Cursors.css';
 
 function App() {
   return (
     <>
-    <div style={{ position: 'absolute', width: '100vw', height: '100vh', zIndex: '-1'}}>
-    <MatrixRainingCode/>
     <button className="z-1 custom-green text-white font-bold py-2 px-4 rounded">Hello</button>
-    </div>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import MatrixRainingCode from './components/MatrixRainingCode.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <div style={{ position: 'absolute', width: '100vw', height: '100vh', zIndex: '-1'}}>
+    <MatrixRainingCode/>
+    </div>
     <App />
   </StrictMode>,
 )
