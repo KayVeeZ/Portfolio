779d44eb39cc7281ee262b40dd2aaa7f7cd4c913
KayVeeZ <kshitijvashisth@gmail.com>
2025-03-30 16:59:50 +0100

    added hire me button

--- src/App.jsx
+++ src/App.jsx
@@ -13,20 +13,31 @@ function App() {
 
   return (
     <div
-      className={`flex flex-col min-h-screen justify-between text-white transition-all duration-700 ease-in ${
-        isVisible ? 'opacity-100' : 'opacity-0'
-      }`}
+      className={`flex flex-col min-h-screen justify-between text-white transition-all duration-700 ease-in ${isVisible ? 'opacity-100' : 'opacity-0'}`}
     >
       {/* Navbar fades in */}
       <Navbar />
 
       {/* Main content fades in */}
-      <div className="flex-grow flex justify-center items-center custom-green space-x-4">
-        <p className="text-6xl">👋</p>
-        <button className="text-6xl custom-green-bg text-white font-bold py-2 px-4 rounded">
-        I'm
-        </button>
-        <p className="text-6xl custom-outline-white">KSHITIJ</p><p className=" text-white text-6xl custom-outline-green">VASHISTH</p><p className=" text-amber-500 text-6xl custom-outline-white">!</p>
+      <div className="flex-grow flex flex-col justify-center items-center custom-green space-y-6">
+        <div className="flex space-x-4">
+          <p className="text-6xl">👋</p>
+          <button className="text-6xl custom-green-bg text-white font-bold py-2 px-4 rounded">
+            I'm
+          </button>
+          <p className="text-6xl custom-outline-white">KSHITIJ</p>
+          <p className="text-white text-6xl custom-outline-green">VASHISTH</p>
+          <p className="text-amber-500 text-6xl custom-outline-white">!</p>
+        </div>
+
+        <div class="slide-in-fade">
+          <span className='curZur'>Hire Me</span>
+        </div>
+
+        <div className="slide-in-fade">
+         <span className="curZur">Aloo Kanda</span>
+        </div>
+
       </div>
 
       {/* Footer fades in */}
