9b127f63dc5c364c153d439f59307761c0bd84be
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-02 14:07:52 +0100

    [feature] added projects section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Intro from './components/Intro';
 import Headline from './components/Headline';
+import Projects from './components/Projects';
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
@@ -17,15 +18,19 @@ function App() {
     <div
       className={`font-custom flex flex-col min-h-screen justify-between text-white transition-all duration-700 ease-in ${isVisible ? 'opacity-100' : 'opacity-0'}`}
     >
+      <section className='flex flex-col min-h-[100vh]'>
       {/* Navbar fades in */}
-      <Navbar />
+      <Navbar className="fixed top-10"/>
 
       {/* Main content fades in */}
       <div className="flex-grow flex flex-col justify-center items-center custom-green select-none">
         <Headline />
         <Intro />
       </div>
-
+      </section>
+      <section className='bg-red-700 min-h-[100vh]'>
+        <Projects/>
+      </section>
       {/* Footer fades in */}
       <Footer className="w-full flex justify-center" />
 
