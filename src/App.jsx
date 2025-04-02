63a0827cb5a0d3239c22cc1178e2296b18749ae4
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-02 14:21:51 +0100

    [update] added brain component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import Intro from './components/Intro';
 import Headline from './components/Headline';
 import Projects from './components/Projects';
+import Brain from './components/Brain';
 
 function App() {
   const [isVisible, setIsVisible] = useState(false);
@@ -18,18 +19,22 @@ function App() {
     <div
       className={`font-custom flex flex-col min-h-screen justify-between text-white transition-all duration-700 ease-in ${isVisible ? 'opacity-100' : 'opacity-0'}`}
     >
-      <section className='flex flex-col min-h-[100vh]'>
-      {/* Navbar fades in */}
-      <Navbar className="fixed top-10"/>
 
-      {/* Main content fades in */}
-      <div className="flex-grow flex flex-col justify-center items-center custom-green select-none">
-        <Headline />
-        <Intro />
-      </div>
+      {/* Navbar fades in */}
+      <Navbar className="fixed top-10" />
+
+      <section className='grid grid-cols-2 min-h-[100vh]'>
+        {/* Main content fades in */}
+        <div className="flex-grow flex flex-col justify-center items-center custom-green select-none ml-[5.5vw]">
+          <Headline />
+          <Intro />
+        </div>
+        <div className="flex-grow flex flex-col justify-center items-center custom-green select-none ml-2 mr-[5.5vw] bg-white">
+          <Brain />
+        </div>
       </section>
       <section className='bg-red-700 min-h-[100vh]'>
-        <Projects/>
+        <Projects />
       </section>
       {/* Footer fades in */}
       <Footer className="w-full flex justify-center" />
---
+++ src/components/Brain.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Brain = () => {
+  return (
+    <>
+    Brain
+    </>
+  )
+}
+
+export default Brain
