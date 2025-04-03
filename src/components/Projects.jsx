9cb2e6ffbd687caf0d6be16deb3293f64355f360
KayVeeZ <kshitijvashisth@gmail.com>
2025-04-03 17:05:22 +0100

    [update] added project descriptions

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,11 +7,20 @@ import project2 from "../assets/projectImages/kvzCareers.png";
 import project3 from "../assets/projectImages/gitUploadAutomation.png";
 import project4 from "../assets/projectImages/blockSnake.png";
 
+// project descriptions
+const desc1 = "The urgency of transitioning to sustainable energy has made solar energy a key solution due to its abundance and environmental benefits. Solar cells are crucial in this shift, offering renewable, clean, cost-effective, and energy-independent power. However, improving solar cell efficiency requires discovering and optimizing suitable materials, a process traditionally slow and labor-intensive. Machine learning (ML) accelerates this by predicting promising materials based on large datasets, optimizing properties, and discovering novel compositions. Leveraging ML in solar cell research can drive innovation and efficiency. To contribute, I led a team, that developed an ML model that predicts stable perovskite materials for solar cells, with the model available on GitHub and deployed online."
+const desc2 ="KayVeeZ Careers Portal is a Flask-based web application that enables job seekers to explore job listings, learn about the company, and apply for positions. It features job listings, an about page, a RESTful API for job data, job details, and an application submission form. The project requires setting up a MySQL database with jobs and applications tables, and dependencies can be installed via pip. The app is powered by Flask for web functionality, SQLAlchemy for database interactions, and MySQL for data storage. The repository is available on GitHub, and the database can be hosted using platforms like Clever Cloud."
+const desc3 = "This Bash script automates updating multiple Git repositories by iterating through a list of directories, staging changes, committing updates, and pushing them to remote repositories. It prints status messages for each update and tracks exit codes to verify success. If any update fails, it identifies and reports the specific repository that encountered an issue, ensuring efficient version control across multiple projects."
+const desc4 = "BlockSnake is a challenging Unity-based twist on the classic snake game, featuring a minimalist blocky design, retro chiptune music, and fast-paced, skill-based gameplay. With precision movement and an unforgiving difficulty curve, it offers an addictive experience for players who love a real test. Can you master the movement and achieve a high score? 🚀🐍"
+
+
+
+
 const projects = [
-  { title: "Solarly", description: "Description of project one. This project is about...", image: project1, link: "https://example.com/project1" },
-  { title: "KVZCareers", description: "Description of project two. It focuses on...", image: project2, link: "https://example.com/project2" },
-  { title: "Git Automation", description: "Description of project three. The main feature is...", image: project3, link: "https://example.com/project3" },
-  { title: "BlockSnake", description: "Description of project four. It was built using...", image: project4, link: "https://example.com/project4" },
+  { title: "Solarly", description: desc1, image: project1, link: "https://predict-solar-cell-materials.onrender.com/" },
+  { title: "KVZCareers", description: desc2, image: project2, link: "https://kayveez-jobz.onrender.com" },
+  { title: "Git Automation", description: desc3, image: project3, link: "https://github.com/KayVeeZ/learning_bash/blob/main/git_up" },
+  { title: "BlockSnake", description: desc4, image: project4, link: "https://kshitijvashisth.itch.io/snake" },
 ];
 
 const Projects = () => {
